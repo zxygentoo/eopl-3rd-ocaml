7fe814ad160065f9a747ed493ca0e48933190c4f
zxygentoo <zxygentoo@gmail.com>
2017-07-20 14:42:23 +0800

    Refactor.

--- src/ch2.ml
+++ src/ch2.ml
@@ -1,21 +1,26 @@
 (* e2.1 *)
 
-module type Bigit =
-sig
+module Bigit : sig
+  (* interfaces *)
   type bigit
   val zero : bigit
   val is_zero : bigit -> bool
-  val successor : bigit -> bigit
-  val predecessor : bigit -> bigit
-end
+  val succ : bigit -> bigit
+  val pred : bigit -> bigit
 
-module Bigit =
-struct
+  (* basic arithmetic *)
+  val add : bigit -> bigit -> bigit
+  val mul : bigit -> bigit -> bigit
+  val fact : bigit -> bigit
+
+  (* helpers *)
+  val of_int : int -> bigit
+  val inspect : bigit -> bigit
+
+end = struct
   type bigit = int list
 
-  exception Exn of string
-
-  (* step >= 1 *)
+  (* must >= 1 *)
   let step : int = 16
 
   let zero = []
@@ -23,81 +28,93 @@ struct
   let rec is_zero n =
     n = zero
 
-  let rec successor n =
+  let rec succ n =
     match n with
     | [] ->
       [1]
-    | [a] when a = step - 1 ->
-      [0; 1]
-    | [a] ->
-      [a + 1]
-    | x::xs when x = step - 1 ->
-      0::successor(xs)
+    | [x;] ->
+      if x = step - 1 then 0::[1] else [x + 1]
     | x::xs ->
-      (x + 1)::xs
+      if x = step - 1 then 0 :: succ xs else x + 1 :: xs
 
-  let rec predecessor n =
+  let rec pred n =
     match n with
     | [] ->
-      raise (Exn "Zero has no predecessor.")
-    | [1] ->
-      []
-    | [a] ->
-      [a - 1]
-    | [0; 1] ->
-      [step - 1]
-    | [0; b] ->
-      [step - 1; b - 1]
-    | [a; b] ->
-      [a - 1; b]
-    | x::xs when x = 0 ->
-      (step - 1)::predecessor(xs)
+      failwith "Zero has no pred."
+    | [x;] ->
+      if x = 1 then [] else [x-1]
+    | [x;y] -> (
+      match x, y with
+      | 0, 1    -> [step - 1]
+      | 0, y'   -> [step - 1; y'-1]
+      | x', y'  -> [x' - 1; y'-1]
+    )
     | x::xs ->
-      (x - 1)::xs
+      if x = 0 then step - 1 :: pred xs else x - 1 :: xs
+
+  let one = succ zero
+
+  let is_one x =
+    is_zero (pred x);;
+
+  let rec add x y =
+    match x, y with
+    | x, y when is_zero x ->
+      y
+    | x, y ->
+      add (pred x) (succ y)
+
+  let rec mul x y =
+    match x, y with
+    | x, y when is_zero x || is_zero y ->
+      zero
+    | x, y when is_one x ->
+      y
+    | x, y ->
+      add y (mul (pred x) y)
+
+  let rec fact n =
+    match n with
+    | x when is_zero x ->
+      one
+    | x when is_one x ->
+      one
+    | x when is_one (pred x) ->
+      x
+    | x ->
+      mul x (fact (pred x))
+
+  let rec of_int i =
+    match i with
+    | x when x < 0 ->
+      failwith "Invalid: negtive ing."
+    | 0 ->
+      zero
+    | x ->
+      add one (of_int (i - 1))
+
+  let to_list n =
+    n
+  
+  let inspect n =
+    List.iter (Printf.printf "%d;") (to_list n);
+    n
 
 end
 ;;
 
 (* tests *)
+
 assert (Bigit.is_zero Bigit.zero = true);;
-assert (Bigit.zero |> Bigit.successor |> Bigit.predecessor = Bigit.zero);;
-(* error *)
-Bigit.zero |> Bigit.predecessor;;
-
-
-let zero = Bigit.zero;;
-let is_zero = Bigit.is_zero;;
-let pred = Bigit.predecessor;;
-let succ = Bigit.successor;;
-let one = succ zero;;
-let is_one x = Bigit.is_zero (Bigit.predecessor x);;
-
-let rec add x y =
-  match x, y with
-  | x, y when is_zero x ->
-    y
-  | x, y ->
-    add (pred x) (succ y)
-;;
-
-let rec mul x y =
-  match x, y with
-  | x, y when is_zero x || is_zero y ->
-    zero
-  | x, y when is_one x ->
-    y
-  | x, y ->
-    add y (mul (pred x) y)
-;;
-
-let rec factorial n =
-  match n with
-  | x when is_zero x ->
-    one
-  | x when is_zero (pred x) ->
-    one
-  | x when is_zero (pred (pred x)) ->
-    x
-  | x ->
-    mul x (factorial (pred x))
-;;
+assert (Bigit.zero |> Bigit.succ |> Bigit.pred = Bigit.zero);;
+Bigit.zero |> Bigit.pred;;
+assert (Bigit.add Bigit.zero (Bigit.succ Bigit.zero) = Bigit.succ Bigit.zero);;
+assert (
+  0
+  |> Bigit.of_int
+  |> Bigit.succ |> Bigit.pred |> Bigit.succ |> Bigit.succ
+  |> Bigit.fact
+  |> Bigit.inspect
+  =
+  (Bigit.zero |> Bigit.succ |> Bigit.succ)
+);;
