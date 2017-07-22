fc6134280fcbe039d49680479cb54a6b12b9ca4e
Alex Zheng <zxygentoo@gmail.com>
2017-07-22 22:01:12 +0800

    Refactor.

--- src/ch2/ch2.5.ml
+++ src/ch2/ch2.5.ml
@@ -14,7 +14,7 @@ module Polish : sig
     | Minus
   type exp =
     | Const of int
-    | Diff of exp * exp
+    | Sub of exp * exp
 
   val parse_one : token list -> exp * (token list)
   val parse : token list -> exp list
@@ -25,18 +25,18 @@ end = struct
     | Minus
   type exp =
     | Const of int
-    | Diff of exp * exp
+    | Sub of exp * exp
 
   let rec parse_one lst =
     match lst with
     | [] ->
       failwith "Invalid expressoin."
-    | Num n :: xs ->
+    | (Num n)::xs ->
       Const n, xs
-    | Minus :: xs ->
+    | Minus::xs ->
       let e1, xs1 = parse_one xs in
       let e2, xs2 = parse_one xs1 in
-      Diff (e1, e2), xs2
+      Sub (e1, e2), xs2
 
   let parse lst =
     let rec parse_aux acc lst =
@@ -64,14 +64,9 @@ let e2 = [Num 42; Num 41];;
 (* (- 42 41) *)
 let e3 = [Minus; Num 42; Num 41];;
 (* (- 42 41 40) *)
-let e4 = [
-  Minus; Minus; Num 42; Num 41; Num 40
-];;
+let e4 = [Minus; Minus; Num 42; Num 41; Num 40];;
 (* (- - 3 2 - 4 - 12 7) *)
-let e5 = [
-  Minus; Minus; Num 3; Num 2; Minus;
-  Num 4; Minus; Num 12; Num 7
-];;
+let e5 = [Minus; Minus; Num 3; Num 2; Minus; Num 4; Minus; Num 12; Num 7];;
 
 parse_one e0;;
 parse_one e1;;
