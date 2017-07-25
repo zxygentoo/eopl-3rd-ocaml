fbc2b48d9d14bddd7b8f857ca88ce73ae88328fd
Alex Zheng <zxygentoo@gmail.com>
2017-07-25 12:47:27 +0800

    Refactor.

--- src/ch3/ch3.2.ml
+++ src/ch3/ch3.2.ml
@@ -1,12 +1,9 @@
-(* Env *)
 module Env : sig
   type 't env = (string * 't) list
   val empty_env : 't env
   val extend_env : string -> 't -> 't env -> 't env
   val apply_env : string -> 't env -> 't
-  (* val is_empty_env : 't env -> bool *)
-  (* val has_binding : string -> 't env -> bool *)
-  (* val extend_env_multiple : (string * 't) list -> 't env -> 't env *)
+
 end = struct
   type 't env = (string * 't) list
 
@@ -35,100 +32,99 @@ end = struct
       failwith "Enviroment has no binding for variable."      
     | Some v ->
       v
+end
+;;
 
-  (*  
-  let has_binding s env =
-    match search_var s env with
-    | None ->
-      false
-    | Some _ ->
-      true
+module LetLang : sig
+  (* Syntax of LET language *)
+  type exp =
+    | Num of int
+    | Sub of (exp * exp)
+    | IsZero of exp
+    | IF of (exp * exp * exp)
+    | Ident of string
+    | LetExp of (string * exp * exp)
+  
+  type program = Prog of exp
+  
+  (* all possible values in LET language *)
+  type value =
+    | Int of int
+    | Bool of bool
+  
+  (* expressed value *)
+  type exp_val = value
+  
+  (* denoted value *)
+  type den_val = value
+  
+  (* interface *)
+  val value_of : exp -> value Env.env -> den_val
 
-  let rec extend_env_multiple alist env =
-    match alist with
-    | [] ->
-      env
-    | (s, v)::xs ->
-      extend_env_multiple xs (extend_env s v env)
- *)
+end = struct
+  (* Syntax of LET language *)
+  type exp =
+    | Num of int
+    | Sub of (exp * exp)
+    | IsZero of exp
+    | IF of (exp * exp * exp)
+    | Ident of string
+    | LetExp of (string * exp * exp)
+  type program = Prog of exp
+  type value =
+    | Int of int
+    | Bool of bool
+  type exp_val = value
+  type den_val = value
+
+  exception Invalid
+
+  let num_val i =
+    Int i
+
+  let bool_val x =
+    Bool x
+
+  let val_to_num =
+    function
+    | Int n -> n
+    | _ -> raise Invalid
+
+  let val_to_bool =
+    function
+    | Bool x -> x
+    | _ -> raise Invalid
+
+  let rec value_of expr env =
+    match expr with
+    | Num i ->
+      Int i
+
+    | Sub (exp1, exp2) ->
+      let exp_to_int exp = val_to_num (value_of exp env) in
+      Int (exp_to_int exp1 - exp_to_int exp2)
+
+    | IsZero exp ->
+      Bool (val_to_num (value_of exp env) = 0)
+
+    | IF (pred_exp, then_exp, else_exp) ->
+      if val_to_bool (value_of pred_exp env)
+      then value_of then_exp env
+      else value_of else_exp env
+
+    | Ident id ->
+      Env.apply_env id env
+
+    | LetExp (id, exp_var, exp_body) ->
+      let new_env = Env.extend_env id (value_of exp_var env) env in
+      value_of exp_body new_env
 end
 ;;
 
 
-(* Syntax of LET language *)
-type exp =
-  | Num of int
-  | Sub of (exp * exp)
-  | IsZero of exp
-  | IF of (exp * exp * exp)
-  | Ident of string
-  | LetExp of (string * exp * exp)
-;;
-
-type program = Prog of exp
-;;
-
-(* all possible values in LET language *)
-type value =
-  | Int of int
-  | Bool of bool
-;;
-
-(* expressed value *)
-type exp_val = value
-;;
-
-(* denoted value *)
-type den_val = value
-;;
-
-exception Invalid
-;;
-
-(* interface *)
-
-let num_val i = Int i
-;;
-
-let bool_val x = Bool x
-;;
-
-let val_to_num =
-  function
-  | Int n -> n
-  | _ -> raise Invalid
-;;
-
-let val_to_bool =
-  function
-  | Bool x -> x
-  | _ -> raise Invalid
-;;
-
-let rec value_of expr env =
-  match expr with
-  | Num i ->
-    Int i
-
-  | Sub (exp1, exp2) ->
-    Int (val_to_num (value_of exp1 env) - val_to_num (value_of exp2 env))
-
-  | IsZero exp ->
-    Bool (val_to_num (value_of exp env) = 0)
-
-  | IF (pred_exp, then_exp, else_exp) ->
-    if val_to_bool (value_of pred_exp env)
-    then value_of then_exp env
-    else value_of else_exp env
-
-  | Ident id ->
-    Env.apply_env id env
-
-  | LetExp (id, exp1, exp2) ->
-    value_of exp2 (Env.extend_env id (value_of exp1 env) env)
-;;
-
 (* tests *)
+
+open LetLang;;
 
 let exp1 = Sub (Num 55, Sub (Num 22, Num 11));;
 let exp2 = Sub (Num 55, Sub (Ident "x", Num 11));;
