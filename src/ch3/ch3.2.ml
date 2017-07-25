67c6fc7fb45d9f170a5e9e387f22cc90b78c6fb9
zxygentoo <zxygentoo@gmail.com>
2017-07-25 19:14:35 +0800

    Add e3.6 3.7 3.8.

--- src/ch3/ch3.2.ml
+++ src/ch3/ch3.2.ml
@@ -1,3 +1,5 @@
+(* e3.6 3.7 3.8 *)
+
 module Env : sig
   type 't env = (string * 't) list
   val empty_env : 't env
@@ -39,8 +41,14 @@ module LetLang : sig
   (* Syntax of LET language *)
   type exp =
     | Num of int
+    | Add of (exp * exp)
     | Sub of (exp * exp)
+    | Mul of (exp * exp)
+    | Div of (exp * exp)
     | IsZero of exp
+    | EQ of (exp * exp)
+    | GT of (exp * exp)
+    | LT of (exp * exp)
     | IF of (exp * exp * exp)
     | Ident of string
     | LetExp of (string * exp * exp)
@@ -65,8 +73,14 @@ end = struct
   (* Syntax of LET language *)
   type exp =
     | Num of int
+    | Add of (exp * exp)
     | Sub of (exp * exp)
+    | Mul of (exp * exp)
+    | Div of (exp * exp)
     | IsZero of exp
+    | EQ of (exp * exp)
+    | GT of (exp * exp)
+    | LT of (exp * exp)
     | IF of (exp * exp * exp)
     | Ident of string
     | LetExp of (string * exp * exp)
@@ -96,16 +110,45 @@ end = struct
     | _ -> raise Invalid
 
   let rec value_of expr env =
+    let exp_to_int expr =
+      val_to_num (value_of expr env)
+    in
+
+    let int_compare expr1 expr2 f =
+      let v1 = value_of expr1 env in
+      let v2 = value_of expr2 env
+      in match v1, v2 with
+      | Int x, Int y -> Bool (f x y)
+      | _ -> raise Invalid
+    in
+    
     match expr with
     | Num i ->
       Int i
 
+    | Add (exp1, exp2) ->
+      Int (exp_to_int exp1 + exp_to_int exp2)
+
     | Sub (exp1, exp2) ->
-      let exp_to_int exp = val_to_num (value_of exp env) in
       Int (exp_to_int exp1 - exp_to_int exp2)
+
+    | Mul (exp1, exp2) ->
+      Int (exp_to_int exp1 * exp_to_int exp2)
+
+    | Div (exp1, exp2) ->
+      Int (exp_to_int exp1 / exp_to_int exp2)
 
     | IsZero exp ->
       Bool (val_to_num (value_of exp env) = 0)
+
+    | EQ (exp1, exp2) ->
+      int_compare exp1 exp2 (=)
+    
+    | LT (exp1, exp2) ->
+      int_compare exp1 exp2 (<)
+
+    | GT (exp1, exp2) ->
+      int_compare exp1 exp2 (>)
 
     | IF (pred_exp, then_exp, else_exp) ->
       if val_to_bool (value_of pred_exp env)
@@ -115,9 +158,12 @@ end = struct
     | Ident id ->
       Env.apply_env id env
 
-    | LetExp (id, exp_var, exp_body) ->
-      let new_env = Env.extend_env id (value_of exp_var env) env in
-      value_of exp_body new_env
+    | LetExp (var_name, var_exp, body_exp) ->
+      let new_env =
+        Env.extend_env var_name (value_of var_exp env) env
+      in
+      value_of body_exp new_env
+
 end
 ;;
 
@@ -126,15 +172,50 @@ end
 
 open LetLang;;
 
-let exp1 = Sub (Num 55, Sub (Num 22, Num 11));;
-let exp2 = Sub (Num 55, Sub (Ident "x", Num 11));;
-let exp3 = IF ((IsZero (Num 0)), exp1, exp2);;
-let exp4 = IF ((IsZero (Num 1)), exp1, exp2);;
+
+let n0 = Num 0;;
+let n1 = Num 11;;
+let n2 = Num 22;;
+
+let env0 = Env.empty_env;;
+
+assert (value_of (IsZero n0) env0 = Bool true);;
+assert (value_of (IsZero n1) env0 = Bool false);;
+
+
+let sub1 = Sub (n0, n1);;
+let sub2 = Sub (n2, sub1);;
+let if1 = IF ((IsZero (Num 0)), sub1, sub2);;
+let if2 = IF ((IsZero (Num 1)), sub1, sub2);;
+
+assert (value_of sub1 env0 = Int (-11));;
+assert (value_of sub2 env0 = Int 33);;
+assert (value_of if1 env0 = Int (-11));;
+assert (value_of if2 env0 = Int 33);;
 
 let env1 = Env.empty_env;;
 let env2 = (Env.extend_env "x" (Int 99) Env.empty_env);;
 
-assert (value_of exp1 env1 = Int 44);;
-assert (value_of exp2 env2 = Int (-33));;
-assert (value_of exp3 env2 = Int 44);;
-assert (value_of exp4 env2 = Int (-33));;
+value_of (GT ((Num 1), (Num 2))) env0;;
+value_of (GT ((Num 2), (Num 1))) env0;;
+
+(* 
+[env : a -> 3]
+let x = 2 * a in x + 1
+*)
+
+value_of (
+  LetExp (
+    "x",
+    Mul (
+      Num (2),
+      Ident ("a")
+    ),
+    Add (
+      Ident ("x"),
+      Num (1)
+    )
+  )
+)
+(Env.extend_env "a" (Int 3) Env.empty_env)
+;;
