4f9d46651349fa1f8a44d777a9af2899471c5849
zxygentoo <zxygentoo@gmail.com>
2017-08-18 15:09:07 +0800

    Refactor.

--- src/ch3/ch3.3.ml
+++ src/ch3/ch3.3.ml
@@ -45,14 +45,21 @@ module FunLang : sig
 
   type exp =
     | Num of int
+    | Boolean of bool
     | Id of id
+    | IsZero of exp
+    | Add of exp * exp
     | Sub of exp * exp
+    | Mul of exp * exp
+    | Div of exp * exp
+    | If of exp * exp * exp
     | Let of id * exp * exp
     | Fun of id * exp
     | App of exp * exp
 
   type value =
     | Int of int
+    | Bool of bool
     | Fn of id * exp * value Env.t
   
   val eval : exp -> value Env.t -> value
@@ -62,14 +69,21 @@ end = struct
 
   type exp =
     | Num of int
+    | Boolean of bool
     | Id of id
+    | IsZero of exp
+    | Add of exp * exp
     | Sub of exp * exp
+    | Mul of exp * exp
+    | Div of exp * exp
+    | If of exp * exp * exp
     | Let of id * exp * exp
     | Fun of id * exp
     | App of exp * exp
 
   type value =
     | Int of int
+    | Bool of bool
     | Fn of id * exp * value Env.t
 
   let int_of_val =
@@ -77,22 +91,64 @@ end = struct
     | Int i -> i
     | _ -> failwith "must be of type Int."    
 
+  let bool_of_val =
+    function
+    | Bool x -> x
+    | _ -> failwith "must be of type Bool."
+
   let fn_of_val =
     function
     | Fn (var, body, env) -> var, body, env
-    | _ -> failwith "must be of type Fn"
+    | _ -> failwith "must be of type Fn."
 
   let rec eval exp env =
+
+    let int_binary_op e1 e2 env op =
+      let ev e = eval e env in
+      Int (op (int_of_val (ev e1)) (int_of_val (ev e2)))
+    in
+  
+    let apply_fn rator rand env =
+      let fn = eval rator env in
+      let arg = eval rand env in
+      let apply fn arg =
+        let var, body, saved_env = fn_of_val fn in
+        let new_env = Env.extend_env var arg saved_env in
+        eval body new_env
+      in
+        apply fn arg
+    in
+
     match exp with
     | Num n ->
       Int n
 
+    | Boolean x ->
+      Bool x
+
     | Id i ->
       Env.apply_env i env
-    
+
+    | IsZero x ->
+      let i = int_of_val (eval x env) in
+      if i = 0 then Bool true else Bool false
+
+    | Add (e1, e2) ->
+      int_binary_op e1 e2 env ( + )
+
     | Sub (e1, e2) ->
-      let ev e = int_of_val (eval e env) in
-      Int (ev e1 - ev e2)
+      int_binary_op e1 e2 env ( - )
+
+    | Mul (e1, e2) ->
+      int_binary_op e1 e2 env ( * )
+
+    | Div (e1, e2) ->
+      int_binary_op e1 e2 env ( / )
+
+    | If (pred, then_exp, else_exp) ->
+      if (bool_of_val (eval pred env))
+      then (eval then_exp env)
+      else (eval else_exp env)
 
     | Let (var, bind, body) ->
       let v = eval bind env in
@@ -103,7 +159,9 @@ end = struct
       Fn (var, body, env)
     
     | App (rator, rand) ->
-      let fn = eval rator env in
+      apply_fn rator rand env
+
+      (* let fn = eval rator env in
       let arg = eval rand env in
       let apply fn arg =
         let var, body, old_env = fn_of_val fn in
@@ -111,42 +169,45 @@ end = struct
         eval body new_env
       in
       apply fn arg
+ *)
 
 end
 ;;
 
 
 (* tests *)
-open FunLang ;;
+module FunLangTest = struct
 
-(*
-  let f = proc (x) -(x,11)
-  in (f (f 77)) 
-  ==> Int 55
-*)
-let e = Let (
-  "f",
-  Fun ("x", Sub (Id "x", Num 11)),
-  App (Id "f", App (Id "f", Num 77))
-)
-in 
-  (assert (eval e Env.empty_env = Int 55))
-;;
+  open FunLang
 
-(* 
-  (
-    proc (f) (f (f 77))
-    proc (x) -(x, 11)
+  (* let f = proc (x) -(x,11) in (f (f 77)) ==> Int 55 *)
+  let exp1 = Let (
+    "f",
+    Fun ("x", Sub (Id "x", Num 11)),
+    App (Id "f", App (Id "f", Num 77))
   )
-  ==> Int 55
-*)
-let f = Fn ("f", App (Id "f", App (Id "f", Num 77)), Env.empty_env) in
-let g = Fn ("x", Sub (Id "x", Num 11), Env.empty_env) in
-let e = App (Id "f", Id "g") in
-let env = 
-  Env.empty_env
-  |> Env.extend_env "f" f
-  |> Env.extend_env "g" g
-in
-  (assert (eval e env = Int 55))
+
+  (* (proc (f) (f (f 77)) proc (x) -(x, 11)) ==> Int 55 *)
+  let exp2 = Let (
+    "f",
+    Fun ("f", App (Id "f", App (Id "f", Num 77))),
+    Let (
+      "g",
+      Fun (("x", Sub (Id "x", Num 11))),
+      App (Id "f", Id "g")
+    )
+  )
+
+  let env0 = Env.empty_env
+
+  let run_test () =
+    Printf.printf "runing FunLang test...\n" ;
+    begin
+      (assert (eval exp1 env0 = Int 55)) ;
+      (assert (eval exp2 env0 = Int 55))    
+    end
+
+end
 ;;
+
+FunLangTest.run_test () ;;
