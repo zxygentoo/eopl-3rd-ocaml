f59cf26d223fe26abbf9556e088b9be3a7013e62
Alex Zheng <zxygentoo@gmail.com>
2017-08-21 23:14:23 +0800

    Refactor.

--- src/ch3/ch3.3.ml
+++ src/ch3/ch3.3.ml
@@ -1,75 +1,60 @@
 (* ch3.3 ProcLang: a letlang with function *)
 
-(* env *)
-module Env : sig
-  type 'a t = (string * 'a) list
-  val empty_env : 't t
-  val extend_env : string -> 'a -> 'a t -> 'a t
-  val apply_env : string -> 'a t -> 'a
-
-end = struct
-  type 'a t = (string * 'a) list
-
-  let empty_env =
-    []
-
-  let is_empty_env env =
-    env = []
-
-  let extend_env s v env =
-    (s, v)::env
-
-  let rec search_var s env=
-    match env with
-    | [] ->
-      None
-    | (var', val')::xs when var' = s ->
-      Some val'
-    | x::xs ->
-      search_var s xs
-
-  let apply_env s env =
-    if is_empty_env env then failwith "Empty enviroment."
-    else match search_var s env with
-      | None ->
-        failwith "Enviroment has no binding for variable."      
-      | Some v ->
-        v
-end
-;;
-
-
-(* FunLang *)
 module FunLang : sig
-  type id = string
 
-  type exp =
-    | Num of int
-    | Boolean of bool
-    | Id of id
-    | IsZero of exp
-    | Add of exp * exp
-    | Sub of exp * exp
-    | Mul of exp * exp
-    | Div of exp * exp
-    | If of exp * exp * exp
-    | Let of id * exp * exp
-    | Fun of id * exp
-    | App of exp * exp
+  (* env *)
+  module Env : sig
+    type 'a t = (string * 'a) list
+    val empty_env : 't t
+    val extend_env : string -> 'a -> 'a t -> 'a t
+    val apply_env : string -> 'a t -> 'a
+  end
 
-  type value =
-    | Int of int
-    | Bool of bool
-    | Fn of id * exp * value Env.t
+  type exp
+
+  type value
 
   val eval : exp -> value Env.t -> value
 
+  (* test *)
+  module Test : sig
+    val run_test : unit -> unit
+  end
+
 end = struct
+
+  (* env *)
+  module Env = struct
+    type 'a t = (string * 'a) list
+
+    let empty_env =
+      []
+
+    let is_empty_env env =
+      env = []
+
+    let extend_env id value env =
+      (id, value) :: env
+
+    let rec search_var id env =
+      match env with
+      | [] -> None
+      | (var', val') :: xs when var' = id -> Some val'
+      | x::xs -> search_var id xs
+
+    let apply_env id env =
+      match search_var id env with
+        | None -> failwith "empty env or has no binding for variable."
+        | Some value -> value
+
+  end
+
+  (* funlang *)
+
   type id = string
 
   type exp =
-    | Num of int
-    | Boolean of bool
+    | IntLit of int
     | Id of id
     | IsZero of exp
     | Add of exp * exp
@@ -91,11 +76,6 @@ end = struct
     | Int i -> i
     | _ -> failwith "must be of type Int."    
 
-  let bool_of_val =
-    function
-    | Bool x -> x
-    | _ -> failwith "must be of type Bool."
-
   let fn_of_val =
     function
     | Fn (var, body, env) -> var, body, env
@@ -108,27 +88,21 @@ end = struct
       Int (op (int_of_val (ev e1)) (int_of_val (ev e2)))
     in
 
-    let apply_fn rator rand env =
-      let fn = eval rator env in
-      let arg = eval rand env in
-      let apply fn arg =
-        let var, body, saved_env = fn_of_val fn in
-        let new_env = Env.extend_env var arg saved_env in
-        eval body new_env
-      in
-      apply fn arg
+    let apply fn arg =
+      let var, body, saved_env = fn_of_val fn in
+      let new_env = Env.extend_env var arg saved_env in
+      eval body new_env
     in
 
     match exp with
-    | Num n -> Int n
-
-    | Boolean x -> Bool x
+    | IntLit n -> Int n
 
     | Id i -> Env.apply_env i env
 
     | IsZero x ->
-      let i = int_of_val (eval x env) in
-      if i = 0 then Bool true else Bool false
+      if (int_of_val (eval x env)) = 0
+      then Bool true
+      else Bool false
 
     | Add (e1, e2) -> int_binary_op e1 e2 env ( + )
 
@@ -139,58 +113,56 @@ end = struct
     | Div (e1, e2) -> int_binary_op e1 e2 env ( / )
 
     | If (pred, then_exp, else_exp) ->
-      if (bool_of_val (eval pred env))
+      if (int_of_val (eval pred env)) = 0
       then (eval then_exp env)
       else (eval else_exp env)
 
     | Let (var, bind, body) ->
-      let v = eval bind env in
-      let new_env = Env.extend_env var v env in
+      let var_value = eval bind env in
+      let new_env = Env.extend_env var var_value env in
       eval body new_env
 
     | Fun (var, body) ->
       Fn (var, body, env)
 
     | App (rator, rand) ->
-      apply_fn rator rand env
+      apply (eval rator env) (eval rand env)
+
+  (* test *)
+  module Test = struct
+
+    (* let f = proc (x) -(x,11) in (f (f 77)) ==> Int 55 *)
+    let exp1 = Let (
+        "f",
+        Fun ("x", Sub (Id "x", IntLit 11)),
+        App (Id "f", App (Id "f", IntLit 77))
+      )
+
+    (* (proc (f) (f (f 77)) proc (x) -(x, 11)) ==> Int 55 *)
+    let exp2 = Let (
+        "f",
+        Fun ("f", App (Id "f", App (Id "f", IntLit 77))),
+        Let (
+          "g",
+          Fun (("x", Sub (Id "x", IntLit 11))),
+          App (Id "f", Id "g")
+        )
+      )
+
+    let env0 = Env.empty_env
+
+    let run_test () =
+      Printf.printf "runing FunLang test...\n" ;
+      begin
+        (assert (eval exp1 env0 = Int 55)) ;
+        (assert (eval exp2 env0 = Int 55))    
+      end
+
+  end
 
 end
 ;;
-
 
 (* tests *)
-module FunLangTest = struct
 
-  open FunLang
-
-  (* let f = proc (x) -(x,11) in (f (f 77)) ==> Int 55 *)
-  let exp1 = Let (
-      "f",
-      Fun ("x", Sub (Id "x", Num 11)),
-      App (Id "f", App (Id "f", Num 77))
-    )
-
-  (* (proc (f) (f (f 77)) proc (x) -(x, 11)) ==> Int 55 *)
-  let exp2 = Let (
-      "f",
-      Fun ("f", App (Id "f", App (Id "f", Num 77))),
-      Let (
-        "g",
-        Fun (("x", Sub (Id "x", Num 11))),
-        App (Id "f", Id "g")
-      )
-    )
-
-  let env0 = Env.empty_env
-
-  let run_test () =
-    Printf.printf "runing FunLang test...\n" ;
-    begin
-      (assert (eval exp1 env0 = Int 55)) ;
-      (assert (eval exp2 env0 = Int 55))    
-    end
-
-end
-;;
-
-FunLangTest.run_test () ;;
+FunLang.Test.run_test () ;;
